90ff20c3ee97a8468a93002d2e94693bd7f2b9c5
i100500 <i100500@e30d8252-6e0f-11de-950b-9fccee66d0d9>
2009-09-06 08:45:11 +0000

    patcher project added
    git-svn-id: http://stopiccot.googlecode.com/svn/trunk@10 e30d8252-6e0f-11de-950b-9fccee66d0d9

---
+++ father/patcher/MainForm.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace patcher
+{
+    using Properties;
+
+    public partial class MainForm : Form
+    {
+        private const string fileName = "D://Program Files//invoice//invoice.exe";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool installUpdate()
+        {
+            if (File.Exists(fileName + ".old")) File.Delete(fileName + ".old");
+            if (File.Exists(fileName)) File.Move(fileName, fileName + ".old");
+            else return false;
+
+            FileStream fileStream = new FileStream(fileName, FileMode.CreateNew);
+            fileStream.Write(Resources.invoice, 0, Resources.invoice.Length);
+            fileStream.Close();
+
+            return true;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            button1.Left = (Width - button1.Width) / 2;
+
+            if (installUpdate())
+            {
+                label1.Text = "Обновление до версии 1.02\nуспешно установлено";
+                button1.Enabled = true;
+            }
+            else
+                label1.Text = "Не удалось установить обновление";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ father/patcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("patcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("patcher")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8b10df38-54a6-40ba-a7f5-44763cae4aed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
