ead75e42bbb03361c058af79ed57d796d4077f47
i100500 <i100500@e30d8252-6e0f-11de-950b-9fccee66d0d9>
2010-07-18 10:39:52 +0000

    libdrag project initial commit
    git-svn-id: http://stopiccot.googlecode.com/svn/trunk@31 e30d8252-6e0f-11de-950b-9fccee66d0d9

---
+++ csharp/libdrag/DragForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace libdrag
+{
+    public class DragForm : Form
+    {
+        protected List<DraggablePoint> dpoints = new List<DraggablePoint>();
+
+        public DragForm()
+        {
+            if (!this.DesignMode)
+            {
+                this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+                this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+                this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+                this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            }
+        }
+
+        public event PaintEventHandler BeforePointsPaint;
+        public event PaintEventHandler AfterPointsPaint;
+
+        private void PaintDraggablePoints(object sender, PaintEventArgs e)
+        {
+            // fuck
+            foreach (DraggablePoint dpoint in this.dpoints)
+            {
+                dpoint.Paint(e);
+            }
+        }
+
+        // huita
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            if (BeforePointsPaint != null)
+            {
+                //MessageBox.Show("BeforePointsPaint");
+                BeforePointsPaint(sender, e);
+            }
+
+            PaintDraggablePoints(sender, e);
+
+            if (AfterPointsPaint != null)
+            {
+                //MessageBox.Show("AfterPointsPaint");
+                AfterPointsPaint(sender, e);
+            }
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            foreach (DraggablePoint dpoint in this.dpoints)
+                dpoint.MouseDown(e);
+
+            Invalidate();
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (DraggablePoint dpoint in this.dpoints)
+                dpoint.MouseMove(e);
+
+            Invalidate();
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            foreach (DraggablePoint dpoint in this.dpoints)
+                dpoint.MouseUp(e);
+
+            Invalidate();
+        }
+    }
+}
---
+++ csharp/libdrag/DraggablePoint.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace libdrag
+{
+    public class DraggablePoint
+    {
+        // Pen to draw all DraggablePoints
+        private static Pen pen = new Pen(Color.Gray, 2.0f);
+
+        // Point position
+        private PointF position;
+
+        // Radius of DraggablePoint
+        private const float PointRadius = 8.0f;
+
+        private static Brush brush = new SolidBrush(Color.FromArgb(0xCC, 0xCC, 0xCC));
+
+        public DraggablePoint(PointF pos)
+        {
+            this.position = pos;
+        }
+
+        // Checks if cursor is over DraggablePoint
+        private bool mouseHit(int x, int y)
+        {
+            double distance = Math.Sqrt((x - position.X) * (x - position.X) + (y - position.Y) * (y - position.Y));
+            return distance < PointRadius;
+        }
+
+        bool drag = false;
+
+        public void Paint(PaintEventArgs e)
+        {
+            RectangleF rect = new RectangleF(position.X - PointRadius, position.Y - PointRadius, 2 * PointRadius, 2 * PointRadius);
+            e.Graphics.FillEllipse(brush, rect);
+            e.Graphics.DrawArc(pen, rect, 0.0f, 360.0f);
+        }
+
+        public void MouseDown(MouseEventArgs e)
+        {
+            if (mouseHit(e.X, e.Y))
+            {
+                drag = true;
+            }
+        }
+
+        public void MouseMove(MouseEventArgs e)
+        {
+            if (drag)
+            {
+                this.position.X = e.X;
+                this.position.Y = e.Y;
+            }
+        }
+
+        public void MouseUp(MouseEventArgs e)
+        {
+            drag = false;
+        }
+
+        public PointF Position
+        {
+            get
+            {
+                return this.position;
+            }
+        }
+    }
+}
