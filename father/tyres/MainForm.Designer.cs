c13ce13f57ecd6a7601d0af75807283b32dcb366
i100500 <i100500@e30d8252-6e0f-11de-950b-9fccee66d0d9>
2010-08-14 09:12:40 +0000

    father: tyres: first working version - 0.7
    git-svn-id: http://stopiccot.googlecode.com/svn/trunk@41 e30d8252-6e0f-11de-950b-9fccee66d0d9

--- father/tyres/MainForm.Designer.cs
+++ father/tyres/MainForm.Designer.cs
@@ -28,26 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.leftFrontTyre = new System.Windows.Forms.NumericUpDown();
             this.rightFrontTyre = new System.Windows.Forms.NumericUpDown();
             this.rightRearTyre = new System.Windows.Forms.NumericUpDown();
             this.leftRearTyre = new System.Windows.Forms.NumericUpDown();
             this.maxTread = new System.Windows.Forms.NumericUpDown();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tуrePrice = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.minTread = new System.Windows.Forms.NumericUpDown();
+            this.resultLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.leftFrontTyre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightFrontTyre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightRearTyre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.leftRearTyre)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.maxTread)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minTread)).BeginInit();
             this.SuspendLayout();
             // 
             // leftFrontTyre
             // 
-            this.leftFrontTyre.DecimalPlaces = 1;
+            this.leftFrontTyre.DecimalPlaces = 2;
             this.leftFrontTyre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.leftFrontTyre.Increment = new decimal(new int[] {
             1,
@@ -58,10 +60,11 @@
             this.leftFrontTyre.Name = "leftFrontTyre";
             this.leftFrontTyre.Size = new System.Drawing.Size(75, 26);
             this.leftFrontTyre.TabIndex = 0;
+            this.leftFrontTyre.ValueChanged += new System.EventHandler(this.updatePrice);
             // 
             // rightFrontTyre
             // 
-            this.rightFrontTyre.DecimalPlaces = 1;
+            this.rightFrontTyre.DecimalPlaces = 2;
             this.rightFrontTyre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.rightFrontTyre.Increment = new decimal(new int[] {
             1,
@@ -72,10 +75,11 @@
             this.rightFrontTyre.Name = "rightFrontTyre";
             this.rightFrontTyre.Size = new System.Drawing.Size(75, 26);
             this.rightFrontTyre.TabIndex = 1;
+            this.rightFrontTyre.ValueChanged += new System.EventHandler(this.updatePrice);
             // 
             // rightRearTyre
             // 
-            this.rightRearTyre.DecimalPlaces = 1;
+            this.rightRearTyre.DecimalPlaces = 2;
             this.rightRearTyre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.rightRearTyre.Increment = new decimal(new int[] {
             1,
@@ -86,10 +90,11 @@
             this.rightRearTyre.Name = "rightRearTyre";
             this.rightRearTyre.Size = new System.Drawing.Size(75, 26);
             this.rightRearTyre.TabIndex = 3;
+            this.rightRearTyre.ValueChanged += new System.EventHandler(this.updatePrice);
             // 
             // leftRearTyre
             // 
-            this.leftRearTyre.DecimalPlaces = 1;
+            this.leftRearTyre.DecimalPlaces = 2;
             this.leftRearTyre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.leftRearTyre.Increment = new decimal(new int[] {
             1,
@@ -100,29 +105,42 @@
             this.leftRearTyre.Name = "leftRearTyre";
             this.leftRearTyre.Size = new System.Drawing.Size(75, 26);
             this.leftRearTyre.TabIndex = 2;
+            this.leftRearTyre.ValueChanged += new System.EventHandler(this.updatePrice);
             // 
             // maxTread
             // 
-            this.maxTread.DecimalPlaces = 1;
+            this.maxTread.DecimalPlaces = 2;
             this.maxTread.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maxTread.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
             this.maxTread.Location = new System.Drawing.Point(205, 28);
             this.maxTread.Name = "maxTread";
             this.maxTread.Size = new System.Drawing.Size(75, 26);
             this.maxTread.TabIndex = 4;
+            this.maxTread.Value = new decimal(new int[] {
+            75,
+            0,
+            0,
+            65536});
+            this.maxTread.ValueChanged += new System.EventHandler(this.updatePrice);
             // 
-            // textBox1
+            // tуrePrice
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(12, 118);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 26);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "100";
+            this.tуrePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tуrePrice.Location = new System.Drawing.Point(12, 118);
+            this.tуrePrice.Name = "tуrePrice";
+            this.tуrePrice.Size = new System.Drawing.Size(156, 26);
+            this.tуrePrice.TabIndex = 5;
+            this.tуrePrice.Text = "50";
+            this.tуrePrice.TextChanged += new System.EventHandler(this.updatePrice);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 6;
@@ -137,40 +155,65 @@
             this.label2.TabIndex = 7;
             this.label2.Text = "Цена колеса";
             // 
-            // numericUpDown1
+            // minTread
             // 
-            this.numericUpDown1.DecimalPlaces = 1;
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDown1.Location = new System.Drawing.Point(205, 60);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(75, 26);
-            this.numericUpDown1.TabIndex = 8;
+            this.minTread.DecimalPlaces = 2;
+            this.minTread.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minTread.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.minTread.Location = new System.Drawing.Point(205, 60);
+            this.minTread.Name = "minTread";
+            this.minTread.Size = new System.Drawing.Size(75, 26);
+            this.minTread.TabIndex = 8;
+            this.minTread.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            65536});
+            this.minTread.ValueChanged += new System.EventHandler(this.updatePrice);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(201, 121);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(57, 20);
+            this.resultLabel.TabIndex = 9;
+            this.resultLabel.Text = "label3";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 162);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.minTread);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tуrePrice);
             this.Controls.Add(this.maxTread);
             this.Controls.Add(this.rightRearTyre);
             this.Controls.Add(this.leftRearTyre);
             this.Controls.Add(this.rightFrontTyre);
             this.Controls.Add(this.leftFrontTyre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "tyres";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Рассчет стоимости колес";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.leftFrontTyre)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightFrontTyre)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.rightRearTyre)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.leftRearTyre)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.maxTread)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minTread)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,10 +226,11 @@
         private System.Windows.Forms.NumericUpDown rightRearTyre;
         private System.Windows.Forms.NumericUpDown leftRearTyre;
         private System.Windows.Forms.NumericUpDown maxTread;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tуrePrice;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown minTread;
+        private System.Windows.Forms.Label resultLabel;
 
 
     }
--- father/tyres/MainForm.cs
+++ father/tyres/MainForm.cs
@@ -17,20 +17,45 @@ namespace tyres
             this.SavePositionToRegistry = true;
         }
 
-        private void leftFrontTyre_ValueChanged(object sender, EventArgs e)
-        {
-            //..
-        }
-
         private void MainForm_Load(object sender, EventArgs e)
         {
-            //...
-            //leftFrontTyre.Text = (4.3).ToString("0.0");
+            leftFrontTyre.Value = (maxTread.Value + minTread.Value) / 2.0m;
+            rightFrontTyre.Value = (maxTread.Value + minTread.Value) / 2.0m;
+            leftRearTyre.Value = (maxTread.Value + minTread.Value) / 2.0m;
+            rightRearTyre.Value  = (maxTread.Value + minTread.Value) / 2.0m;
         }
 
-        private void leftFrontTyre_SelectedItemChanged(object sender, EventArgs e)
+        private void updatePrice(object sender, EventArgs e)
         {
-            this.Text += ".";
+            try
+            {
+                decimal delta = maxTread.Value - minTread.Value;
+                decimal result = 0;
+
+                //this.Text = leftFrontTyre.Value.ToString() + " " + rightFrontTyre.Value.ToString() +
+                //    " " + leftRearTyre.Value.ToString() + " " + rightRearTyre.Value.ToString() + " " + maxTread.Value.ToString() + " " + minTread.Value.ToString();
+
+                result += (leftFrontTyre.Value - minTread.Value) / delta;
+                result += (rightFrontTyre.Value - minTread.Value) / delta;
+                result += (leftRearTyre.Value - minTread.Value) / delta;
+                result += (rightRearTyre.Value - minTread.Value) / delta;
+
+                result -= 4 * 0.5m;
+                result *= 4 * decimal.Parse(tуrePrice.Text);
+                resultLabel.Text = result.ToString("0.00");
+
+                if (result > 0.0m)
+                    resultLabel.ForeColor = Color.Green;
+                else if (result < 0.0m)
+                    resultLabel.ForeColor = Color.Red;
+                else
+                    resultLabel.ForeColor = Color.Black;
+            }
+            catch
+            {
+                resultLabel.Text = "Ошибка";
+                resultLabel.ForeColor = Color.Red;
+            }
         }
     }
 }
