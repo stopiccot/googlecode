ed51941f67b436b94e77e2abb26d650cbd35bae1
i100500@gmail.com <i100500@gmail.com@e30d8252-6e0f-11de-950b-9fccee66d0d9>
2011-08-26 22:35:40 +0000

    SpaceInvaders
    git-svn-id: http://stopiccot.googlecode.com/svn/trunk@56 e30d8252-6e0f-11de-950b-9fccee66d0d9

---
+++ different/SpaceInvaders/InvaderForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class InvaderForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.saveButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.AutoSize = false;
+			this.trackBar1.BackColor = System.Drawing.Color.Black;
+			this.trackBar1.Location = new System.Drawing.Point(407, 12);
+			this.trackBar1.Maximum = 20;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(233, 34);
+			this.trackBar1.TabIndex = 0;
+			this.trackBar1.Value = 6;
+			this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+			// 
+			// saveButton
+			// 
+			this.saveButton.Location = new System.Drawing.Point(12, 12);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(75, 23);
+			this.saveButton.TabIndex = 1;
+			this.saveButton.Text = "save";
+			this.saveButton.UseVisualStyleBackColor = true;
+			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// InvaderForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(652, 494);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.trackBar1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.KeyPreview = true;
+			this.MaximizeBox = false;
+			this.Name = "InvaderForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Space Invaders";
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Button saveButton;
+	}
+}
+
---
+++ different/SpaceInvaders/InvaderForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+/**
+ * <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1"
+     baseProfile="full"
+     xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink"
+     xmlns:ev="http://www.w3.org/2001/xml-events"
+     width="100%" height="100%">
+<rect fill="white" x="0" y="0" width="100%" height="100%" />
+<rect fill="silver" x="0" y="0" width="100%" height="100%" rx="1em"/>
+</svg>
+ */
+
+namespace WindowsFormsApplication1
+{
+	public partial class InvaderForm : Form
+	{
+		public InvaderForm()
+		{
+			InitializeComponent();
+		}
+
+		private void DrawSquare(Graphics g, int x, int y, float squarePadding)
+		{
+			const float padding = 50.0f;
+			const float squareWidth = 50.0f;
+			g.FillRectangle(Brushes.White, padding + squareWidth * x + squarePadding / 2.0f, padding + squareWidth * y + squarePadding / 2.0f, squareWidth - squarePadding, squareWidth - squarePadding);
+		}
+
+		private bool noPadding = false;
+		private float squarePadding = 6.0f;
+		private int[,] grid = new int[,]{
+				{ 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+				{ 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0 },
+				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1 },
+				{ 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0 },
+			};
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Graphics g = Graphics.FromHwnd(this.Handle);
+			g.Clear(Color.Black);
+
+			for (int x = 0; x < 11; x++)
+				for (int y = 0; y < 8; y++)
+					if (grid[y, x] != 0)
+						DrawSquare(g, x, y, noPadding ? 0.0f : squarePadding);
+		}
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			noPadding = !noPadding;
+			this.Invalidate();
+		}
+
+		private void trackBar1_ValueChanged(object sender, EventArgs e)
+		{
+			this.squarePadding = this.trackBar1.Value;
+			this.Invalidate();
+		}
+
+		private void saveButton_Click(object sender, EventArgs e)
+		{
+			const float padding = 50.0f;
+			const float squareWidth = 50.0f;
+
+			TextWriter svg = new StreamWriter("D:\\invaders.svg");
+			svg.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+			svg.WriteLine("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">");
+			svg.WriteLine("<svg version=\"1.1\" baseProfile=\"full\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:ev=\"http://www.w3.org/2001/xml-events\" width=\"100%\" height=\"100%\">");
+			svg.WriteLine(("<rect fill=\"black\" x=\"0\" y=\"0\" width=\"" + this.Width.ToString() + "\" height=\"" + this.Height.ToString() + "\" />").Replace(',', '.'));
+			for (int x = 0; x < 11; x++)
+			{
+				for (int y = 0; y < 8; y++)
+				{
+					if (grid[y, x] != 0)
+					{
+						float left = (padding + squareWidth * x + squarePadding / 2.0f);
+						float top = (padding + squareWidth * y + squarePadding / 2.0f);
+						float width = (squareWidth - squarePadding);
+						float height = (squareWidth - squarePadding);
+
+						string ss = "<rect fill=\"white\" x=\"" + left.ToString() + "\" y=\"" + top.ToString() + "\" width=\"" + width.ToString() + "\" height=\"" + height.ToString() + "\" />";
+						svg.WriteLine(ss.Replace(',', '.'));
+					}
+				}
+			}
+			svg.WriteLine("</svg>");
+			svg.Close();
+		}
+	}
+}
